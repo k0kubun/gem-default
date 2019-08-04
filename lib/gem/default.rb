e67c274c9927d783091b722b2d7f787063e683d2
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-04 12:37:57 +0900

    Initialize gemspec

--- lib/gem/default.rb
+++
@@ -1,8 +0,0 @@
-require "gem/default/version"
-
-module Gem
-  module Default
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/gem/default/version.rb
+++
@@ -1,5 +0,0 @@
-module Gem
-  module Default
-    VERSION = "0.1.0"
-  end
-end
