cc646b6250a85a9c33db593f8b4edb754d3f865d
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-04 12:32:05 +0900

    bundle gem gem-default

---
+++ lib/gem/default.rb
@@ -0,0 +1,8 @@
+require "gem/default/version"
+
+module Gem
+  module Default
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/gem/default/version.rb
@@ -0,0 +1,5 @@
+module Gem
+  module Default
+    VERSION = "0.1.0"
+  end
+end
